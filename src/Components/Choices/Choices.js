53d0f7887f3d59f52acef6c52cb0757dd253a155
matheus <matheusfelipe@opus-software.com.br>
2023-06-17 02:39:45 -0300

    Home page and choices component

---
+++ src/Components/Choices/Choices.js
@@ -0,0 +1,19 @@
+import './Choices.scss'
+
+const Choices = () =>{
+    return(
+        <>
+        <div className="choices">
+            <div className="choicesBtn">
+                <button> Relax</button>
+                <button>Em Trânsito</button>
+                <button>Energia</button>
+                <button>Treino</button>
+                <button>Foco</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Choices
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "../../Components/Navbar/Navbar"
+import Choices from "../../Components/Choices/Choices"
+
+const Home=() =>{
+    return(
+        <>
+        <Navbar/>
+        <Choices/>
+        </>
+    )
+}
+
+export default Home
