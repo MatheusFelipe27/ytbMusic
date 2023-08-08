a171ccafb38c8d90b29b84ede14a042d368da03d
Matheus Felipe <mfs5@cin.ufpe.br>
2023-08-08 19:55:45 -0300

    collapsed navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,13 +4,21 @@ import { TfiSearch } from "react-icons/tfi";
 import { FiMenu } from "react-icons/fi";
 import { useEffect, useState } from "react";
 import {useDispatch, useSelector} from 'react-redux'
+import NavbarCollapsed from "./NavbarCollapsed/NavbarCollapsed";
 
 const Navbar = () => {
   const [navbarFixed, setNavbarFixed] = useState(false);
+  const [navbarClass, setNavbarClass] = useState("navbar")
   const [searchColor, setSearchColor] = useState("gray")
 
   const dispatch = useDispatch()
   const menuIsCollapsed = useSelector(state => state.collapsedMenu)
+
+  useEffect(()=>{
+    setNavbarClass(
+      `${navbarFixed? 'navbarFixed' : 'navbar'}${menuIsCollapsed.collapsedMenu? 'Collapsed': ''}`
+    )
+},[menuIsCollapsed, navbarFixed])
 
   const collapseSideBar = () =>{
     dispatch({type: 'COLLAPSED_MENU'})
@@ -55,35 +63,41 @@ const Navbar = () => {
 
   return (
     <>
-      <nav className={navbarFixed ? "navbarFixed" : "navbar"}>
-        <div className="leftSide">
-          <div className="menuOutside">
-            <span className="menu" onClick={collapseSideBar}>
-              {" "}
-              <FiMenu size={"22px"} color={"#F0000"} />
-            </span>
-          </div>
-          <div className="logo">
-            <VscPlayCircle size={"18px"} color={"#edf0f5"} />
-          </div>
-          <span className="logoName">Music</span>
-          <div className="search">
-            <input
-              className="searchInput"
-              onFocus={changeSearchColorFocus}
-              onBlur= {changeSearchColorBlur}
-              placeholder="Pesquise músicas, álbuns, artistas, podcasts"
-            />
-            <span className="searchIcon">
-              <TfiSearch size={"18px"} color={searchColor} />
-            </span>
-          </div>
-        </div>
-        <div className="rightDiv">
-          <div className="login">
-            <span>M</span>
-          </div>
-        </div>
+      <nav className={navbarClass}>
+        {navbarClass.includes('Col')?
+          <NavbarCollapsed/>
+          :
+          <>
+            <div className="leftSide">
+              <div className="menuOutside">
+                <span className="menu" onClick={collapseSideBar}>
+                  {" "}
+                  <FiMenu size={"22px"} color={"#F0000"} />
+                </span>
+              </div>
+              <div className="logo">
+                <VscPlayCircle size={"18px"} color={"#edf0f5"} />
+              </div>
+              <span className="logoName">Music</span>
+              <div className="search">
+                <input
+                  className="searchInput"
+                  onFocus={changeSearchColorFocus}
+                  onBlur= {changeSearchColorBlur}
+                  placeholder="Pesquise músicas, álbuns, artistas, podcasts"
+                />
+                <span className="searchIcon">
+                  <TfiSearch size={"18px"} color={searchColor} />
+                </span>
+              </div>
+            </div>
+            <div className="rightDiv">
+              <div className="login">
+                <span>M</span>
+              </div>
+            </div>
+          </>
+        }
       </nav>
     </>
   );
