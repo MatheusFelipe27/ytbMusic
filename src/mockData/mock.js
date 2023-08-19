a432c251a114991e7f183bd2a48247a4a4a3aa3a
Matheus Felipe <mfs5@cin.ufpe.br>
2023-08-19 19:00:43 -0300

    mocked data file

---
+++ src/mockData/mock.js
@@ -0,0 +1,73 @@
+export const songs = [
+    { name: 'Euphoria Symphony', artist: 'Luna Melodiosa', image: 'cds' },
+    { name: 'Retro Grooves', artist: 'Neon Beats', image: 'vinil' },
+    { name: 'Whispering Dreams', artist: 'Aurora Harmonics', image: 'cds' },
+    { name: 'Jazz Noir', artist: 'Midnight Serenaders', image: 'tapes' },
+    { name: 'Enchanted Melodies', artist: 'Celestial Sounds', image: 'cds' },
+    { name: 'Electric Dreams', artist: 'Synthwave Explorers', image: 'tapes' },
+    { name: 'Serenity Sonata', artist: 'Dusk Ensemble', image: 'cds' },
+    { name: 'Urban Rhythms', artist: 'City Pulse', image: 'tapes' },
+    { name: 'Mystic Overture', artist: 'Ethereal Ensemble', image: 'cds' },
+    { name: 'Luminous Echoes', artist: 'Starry Resonance', image: 'tapes' },
+    { name: 'Whimsical Waltz', artist: 'Carnival Orchestra', image: 'cds' },
+    { name: 'Galactic Groove', artist: 'Cosmic Travelers', image: 'vinil' },
+    { name: 'Silent Reverie', artist: 'Moonlit Reflections', image: 'cds' },
+    { name: 'Neon Nocturne', artist: 'Synthetic Nightlife', image: 'tapes' },
+    { name: 'Emerald Elegy', artist: 'Verdant Melodies', image: 'vinil' }
+  ];
+
+  export const songsTwo = [
+    { name: 'Summer Breeze', artist: 'Sunset Serenaders', image: 'tapes' },
+    { name: 'Moonlit Melodies', artist: 'Starry Nights Orchestra', image: 'cds' },
+    { name: 'Dancing Raindrops', artist: 'Harmony Echoes', image: 'vinil' },
+    { name: 'Jazz Café', artist: 'Midnight Ensemble', image: 'notes' },
+    { name: 'Enchanted Forest', artist: 'Mystic Harmonies', image: 'tapes' },
+    { name: 'Neon Lights', artist: 'Synthwave Seekers', image: 'cds' },
+    { name: 'Serene Sunset', artist: 'Twilight Echoes', image: 'vinil' },
+    { name: 'City Vibes', artist: 'Urban Pulse', image: 'notes' },
+    { name: 'Crystal Whispers', artist: 'Celestial Harmony', image: 'tapes' },
+    { name: 'Starlit Serenade', artist: 'Cosmic Melodies', image: 'cds' },
+    { name: 'Carnival Fantasy', artist: 'Joyful Orchestras', image: 'notes' },
+    { name: 'Galactic Adventure', artist: 'Interstellar Travelers', image: 'vinil' },
+    { name: 'Midnight Reverie', artist: 'Lunar Reflections', image: 'tapes' },
+    { name: 'Synthetic Dreams', artist: 'Electric Nocturnes', image: 'cds' },
+    { name: 'Emerald Echoes', artist: 'Verdant Harmonics', image: 'notes' }
+  ];
+  
+  export const songsThree = [
+    { name: 'Midnight Serenade', artist: 'Luna Nightshade', image: 'cds' },
+    { name: 'Echoes of Eternity', artist: 'Astral Harmonies', image: 'tapes' },
+    { name: 'Dance of the Stars', artist: 'Celestial Dancers', image: 'notes' },
+    { name: 'Urban Grooves', artist: 'City Beats', image: 'vinil' },
+    { name: 'Dreamer\'s Lullaby', artist: 'Serene Dreamscapes', image: 'tapes' },
+    { name: 'Neon Reflections', artist: 'Synthwave Explorers', image: 'cds' },
+    { name: 'Enchanted Gardens', artist: 'Mystic Melodies', image: 'notes' },
+    { name: 'Twilight Jazz', artist: 'Nightfall Quartet', image: 'vinil' },
+    { name: 'Crystal Whispers', artist: 'Harmonic Resonance', image: 'cds' },
+    { name: 'Starry Nocturne', artist: 'Celestial Echoes', image: 'tapes' },
+    { name: 'Carnival Adventure', artist: 'Joyful Performers', image: 'vinil' },
+    { name: 'Galactic Serenity', artist: 'Cosmic Symphony', image: 'notes' },
+    { name: 'Moonlit Reverie', artist: 'Silvermoon Ensemble', image: 'tapes' },
+    { name: 'Synthetic Dreams', artist: 'Electric Waves', image: 'cds' },
+    { name: 'Emerald Harmony', artist: 'Verdant Serenades', image: 'vinil' }
+  ];
+
+  export const songsFour = [
+    { name: 'Celestial Reverie', artist: 'Aurora Echoes', image: 'vinil' },
+    { name: 'City Lights', artist: 'Urban Harmony', image: 'notes' },
+    { name: 'Whispering Winds', artist: 'Mystic Melodics', image: 'cds' },
+    { name: 'Jazzed Up', artist: 'Midnight Jazz Trio', image: 'tapes' },
+    { name: 'Ethereal Dreams', artist: 'Dreamweavers', image: 'vinil' },
+    { name: 'Synthetic Symphony', artist: 'Digital Serenades', image: 'notes' },
+    { name: 'Serenity Falls', artist: 'Cascading Harmony', image: 'cds' },
+    { name: 'Neon Nights', artist: 'Synthwave Sensation', image: 'tapes' },
+    { name: 'Starlight Serenade', artist: 'Stellar Ensemble', image: 'cds' },
+    { name: 'Enchanted Voyage', artist: 'Mystic Journeys', image: 'vinil' },
+    { name: 'Carnival Rhythms', artist: 'Joyful Beats', image: 'notes' },
+    { name: 'Galactic Melancholy', artist: 'Cosmic Echoes', image: 'cds' },
+    { name: 'Moonlit Sonata', artist: 'Lunar Harmonics', image: 'tapes' },
+    { name: 'Digital Dreams', artist: 'Electric Soundscapes', image: 'vinil' },
+    { name: 'Emerald Twilight', artist: 'Verdant Nocturnes', image: 'notes' }
+  ];
+  
+  
